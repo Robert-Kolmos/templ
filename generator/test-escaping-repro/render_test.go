c18145f12e9d266f255df9c1253227f0b58d6768
Robert Kolmos <robertkolmos@gmail.com>
2024-05-02 18:38:00 -0700

    add a test demonstrating how to acheive the desired behavior in #624

---
+++ generator/test-escaping-repro/render_test.go
@@ -0,0 +1,24 @@
+package testescapingrepro
+
+import (
+	"testing"
+
+	_ "embed"
+
+	"github.com/a-h/templ/generator/htmldiff"
+)
+
+//go:embed expected.html
+var expected string
+
+func Test(t *testing.T) {
+	component := render()
+
+	diff, err := htmldiff.Diff(component, expected)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff != "" {
+		t.Error(diff)
+	}
+}
